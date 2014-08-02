aaa8309d3e1ca5dd4678da8f4fc4002ceda8d352
Your Actual Name <Your Actual Email>
2014-08-02 15:58:12 -0700

    heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
-    :aws_access_key_id      => 'AKIAJ26NZ67BW3S66K2Q',                        # required
-    :aws_secret_access_key  => '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',                        # required
+    :aws_access_key_id      => ENV["aws_access_key_id"],                        # required
+    :aws_secret_access_key  => ENV["aws_secret_access_key"],
     
   }
   config.fog_directory  = 'yelpdemojq'                     # required
